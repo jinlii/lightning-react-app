7e8d2e2521ef11704c22a4e4b45141d3d7bf251b
Jin <jili@heidrick.com>
2019-03-11 21:06:24 -0500

    update posgres db url

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    databaseURL: process.env.DATABASE_URL || "postgres://ccoenraets@localhost/realty"
+    databaseURL: process.env.DATABASE_URL || "postgres://postgres:p!234@localhost:5434/realty"
 
 };
